6ae391ed9ce879935816ea74f68f9c5dbfca6f61
keelann95 <sephen.mwaniki@student.moringaschool.com>
2024-08-18 21:16:13 +0300

    modified js file

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,7 @@
 // Your code here
+// utils.js
+
+export function currentAgeForBirthYear(birthYear) {
+    const fixedYear = 2022;  // The year the test was written for
+    return fixedYear - birthYear;
+  }
